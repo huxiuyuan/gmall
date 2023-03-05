5682e09221b0aad27ae0faf75e878ca0b57c570a
胡修远 <huxiuyuan@hczhiyun.com>
2023-03-05 21:19:07 +0800

    商品管理-商品列表：根据分类id查询分类下的属性分组及具体属性

--- gmall-pms/src/main/java/com/atguigu/gmall/pms/service/impl/AttrGroupServiceImpl.java
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/service/impl/AttrGroupServiceImpl.java
@@ -9,6 +9,7 @@ import com.atguigu.gmall.pms.mapper.AttrMapper;
 import com.atguigu.gmall.pms.service.AttrGroupService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -51,30 +52,14 @@ public class AttrGroupServiceImpl extends ServiceImpl<AttrGroupMapper, AttrGroup
      */
     @Override
     public List<AttrGroupEntity> queryAttrGroupAndAttrByCid(Long cid) {
-        // 方法一：
-//        List<AttrGroupEntity> attrGroupEntities = this.list(new QueryWrapper<AttrGroupEntity>().eq("categroy_id", cid));
-//        if (CollectionUtils.isEmpty(attrGroupEntities)){
-//            return null;
-//        }
-//        attrGroupEntities.forEach(attrGroupEntity -> {
-//            List<AttrEntity> attrEntities = attrMapper.selectList(new QueryWrapper<AttrEntity>().eq("group_id", attrGroupEntity).eq("type",1));
-//            attrGroupEntity.setAttrEntities(attrEntities);
-//        });
-//        return attrGroupEntities;
-
-        // 自己写的垃圾代码
-        QueryWrapper<AttrGroupEntity> query = new QueryWrapper<>();
-        query.eq("category_id",cid);
-        List<AttrGroupEntity> attrGroupEntities = attrGroupMapper.selectList(query);
-        if (CollectionUtils.isEmpty(attrGroupEntities)){
+        List<AttrGroupEntity> attrGroupEntities = this.attrGroupMapper.selectList(Wrappers.<AttrGroupEntity>lambdaQuery().eq(AttrGroupEntity::getCategoryId, cid));
+        if (CollectionUtils.isEmpty(attrGroupEntities)) {
             return null;
         }
-        for (AttrGroupEntity t : attrGroupEntities){
-            Long id = t.getId();
-            QueryWrapper<AttrEntity> queryWrapper = new QueryWrapper<>();
-            List<AttrEntity> entityList = attrMapper.selectList(queryWrapper.eq("group_id", id).eq("type",1));
-            t.setAttrEntities(entityList);
-        }
+        attrGroupEntities.forEach(e -> {
+            List<AttrEntity> attrEntities = this.attrMapper.selectList(Wrappers.<AttrEntity>lambdaQuery().eq(AttrEntity::getGroupId, e.getId()).eq(AttrEntity::getType, 1));
+            if (!CollectionUtils.isEmpty(attrEntities)) e.setAttrEntities(attrEntities);
+        });
         return attrGroupEntities;
     }
 
