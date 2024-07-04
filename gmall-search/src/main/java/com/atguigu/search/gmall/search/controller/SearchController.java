5eede434c3841ce989e3d3755cedcf75c0c859e1
胡修远 <huxiuyuan@hczhiyun.com>
2024-07-04 23:33:18 +0800

    搜索页面前后端联调

--- gmall-search/src/main/java/com/atguigu/search/gmall/search/controller/SearchController.java
+++ gmall-search/src/main/java/com/atguigu/search/gmall/search/controller/SearchController.java
@@ -5,15 +5,16 @@ import com.atguigu.search.gmall.search.pojo.SearchParamVo;
 import com.atguigu.search.gmall.search.pojo.SearchResponseVo;
 import com.atguigu.search.gmall.search.service.SearchService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author huXiuYuan
  * @Description：搜素服务控制层
  * @date 2024/7/3 17:21
  */
-@RestController
+@Controller
 public class SearchController {
 
     @Autowired
@@ -25,8 +26,11 @@ public class SearchController {
      * @param searchParamVo 查询条件
      */
     @GetMapping("search")
-    public ResponseVo<SearchResponseVo> search(SearchParamVo searchParamVo) {
+    public ResponseVo<SearchResponseVo> search(SearchParamVo searchParamVo, Model model) {
         SearchResponseVo searchResponseVo = this.searchService.search(searchParamVo);
+
+        model.addAttribute("response", searchResponseVo);
+        model.addAttribute("searchParam", searchParamVo);
 
         return ResponseVo.ok(searchResponseVo);
     }
