a9f2849b4a2f2463e9ce093d24a4a47da68359a6
胡修远 <huxiuyuan@hczhiyun.com>
2024-07-26 23:11:43 +0800

    搭建购物车工程

---
+++ gmall-card/src/test/java/com/atguigu/gmall/card/GmallCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall.card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallCardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
